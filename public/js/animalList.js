18c1b311e4c7586f789136ea497f7bbeb95cc3ed
alyssackieffer <alyssackieffer@Alyssas-MacBook-Pro.local>
2018-11-19 17:46:57 -0500

    Style changes

--- public/js/animalList.js
+++ public/js/animalList.js
@@ -1,27 +1,29 @@
 class AnimalList extends React.Component {
   render (){
     return (
-      <table>
-        <tbody>
-        {this.props.animals.map((animal, index) => {
-          return (
-            <tr>
-            <td onClick={()=> { this.props.getAnimal(animal); this.props.toggleState('animalListIsVisible', 'animalIsVisible')}}>
-                <img src={animal.image} alt={animal.title} />
-              </td>
-              <td className='animalList' onClick={()=> { this.props.getAnimal(animal); this.props.toggleState('animalListIsVisible', 'animalIsVisible')}}>
-              </td>
-              <td>
-                  <button className='animalList' onClick={()=> { this.props.getAnimal(animal); this.props.toggleState('animalListIsVisible', 'animalIsVisible')}}>Edit</button>
-              </td>
-              <td>
-                  <button className='animalList' onClick={() => this.props.deleteAnimal(animal,index)}>Delete</button>
-              </td>
-            </tr>
-          )
-        })}
-        </tbody>
-      </table>
+      <div className="mainPage">
+        <table>
+          <tbody>
+          {this.props.animals.map((animal, index) => {
+            return (
+              <tr className="mainPage">
+                <td onClick={()=> { this.props.getAnimal(animal); this.props.toggleState('animalListIsVisible', 'animalIsVisible')}}>
+                    <img src={animal.image} alt={animal.title} />
+                  </td>
+                  <td className='animalList' onClick={()=> { this.props.getAnimal(animal); this.props.toggleState('animalListIsVisible', 'animalIsVisible')}}>
+                  </td>
+                  <td>
+                      <button className='animalListButton' onClick={()=> { this.props.getAnimal(animal); this.props.toggleState('animalListIsVisible', 'animalIsVisible')}}>Edit</button>
+                  </td>
+                  <td>
+                      <button className='animalListButton' onClick={() => this.props.deleteAnimal(animal,index)}>Delete</button>
+                  </td>
+              </tr>
+            )
+          })}
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
